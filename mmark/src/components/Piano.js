06f91a2df10abc9012fa20ec42bd48ade9fad521
antonyj <antony@kaist.ac.kr>
2021-05-26 04:25:29 +0900

    Try single-soundfontplayer piano - Add InstrumentAudio

--- mmark/src/components/Piano.js
+++ mmark/src/components/Piano.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 // const keyNames = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 // const octaveNames = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const allNotes = ["C3","C#3","D3","D#3","E3","F3","F#3","G3","G#3","A3","A#3","B3",
-                  "C4","C#4","D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4",];
+                  "C4","C#4","D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4","C5"];
 
 const PianoWrapper = styled.div `
   display: flex;
