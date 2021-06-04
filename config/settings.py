229d000bd4ec9453b9c2b0ba9420b258e6e7bd24
HoneyTt <honeytt21@kaist.ac.kr>
2021-06-05 04:35:34 +0900

    isAdminUser

--- config/settings.py
+++ config/settings.py
@@ -140,6 +140,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAdminUser'
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
