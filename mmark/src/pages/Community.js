e05ec5712d4d12511593a6af03f3192cd0ccffa5
HoneyTt <honeytt21@kaist.ac.kr>
2021-06-05 01:52:05 +0900

    static ip correction

--- mmark/src/pages/Community.js
+++ mmark/src/pages/Community.js
@@ -14,7 +14,7 @@ class Community extends Component {
 
     async componentDidMount() {
         try {
-            const res_articles = await fetch('http://3.36.217.441:8000/api/articles');
+            const res_articles = await fetch('http://3.36.217.44:8000/api/articles');
             const articles = await res_articles.json();
             this.setState({
                 articles
