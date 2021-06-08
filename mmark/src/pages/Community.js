f4aec140bce7133529cf0984ebcb3cc1bde8dac0
황혜원 <imhywn@kaist.ac.kr>
2021-06-08 10:50:01 +0900

    Fix: community without login

--- mmark/src/pages/Community.js
+++ mmark/src/pages/Community.js
@@ -218,10 +218,13 @@ class Community extends Component {
                 <button className='newPostButton' onClick={this.handleNewPost}>{t("community.new_post")}</button>
                 {
                     this.state.curUser
-                    ?<CommunityBlock className="listWrapper" articles={this.state.articles} users={this.state.users} curUser={this.state.curUser} tags={this.state.tags} handleDelete={this.handleDelete} handleFav={this.handleFav}/>
-                    :<CommunityBlock className="listWrapper" articles={this.state.articles} users={this.state.users} tags={this.state.tags} handleFav={this.handleFav}/>
+                    ?<>
+                        <CommunityBlock className="listWrapper" articles={this.state.articles} users={this.state.users} curUser={this.state.curUser} tags={this.state.tags} handleDelete={this.handleDelete} handleFav={this.handleFav}/>
+                        <MyProfile />
+                    </>
+                    :
+                        <CommunityBlock className="listWrapper" articles={this.state.articles} users={this.state.users} tags={this.state.tags} handleFav={this.handleFav}/>
                 }
-                <MyProfile className="community-myprofile"/>
             </div>
             
         );
